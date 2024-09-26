d60fa46ee642fb5fec25747ada886afb22cfed8f
Praveen Singh <praveensingh.199@gmail.com>
2024-09-26 21:08:30 +0530

    spring project commit

---
+++ java-project/src/main/java/com/spring/boot/model/Employee.java
@@ -0,0 +1,33 @@
+package com.spring.boot.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Employee {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  @Column(name = "email_id")
+  private String emailId;
+
+}
---
+++ java-project/src/main/java/com/spring/boot/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.spring.boot.repository;
+
+import com.spring.boot.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
